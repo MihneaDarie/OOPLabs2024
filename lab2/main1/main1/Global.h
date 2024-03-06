8b5ef2c3f8b5a3985451da4c7fa4be6b7dd295ca
Darie Mihnea A2 <153534353+MihneaDarie@users.noreply.github.com>
2024-03-06 13:36:28 +0200

    Problema_2_Lab2

--- lab2/main1/main1/Global.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "student.h"
-
-int CompareMGrade(student* a, student* b);
-int CompareEGrade(student* a, student* b);
-int CompareHGrade(student* a, student* b);
-int CompareAverageGrade(student* a, student* b);
-int CompareName(student* a, student* b);
