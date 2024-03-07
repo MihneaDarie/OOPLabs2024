9822994d7dd3afcdc323099275432da1513304e9
Darie Mihnea A2 <153534353+MihneaDarie@users.noreply.github.com>
2024-03-07 15:05:26 +0200

    Delete lab2/main/main directory

--- lab2/main/main/global.cpp
+++
@@ -1,8 +0,0 @@
-#include "global.h"
-#include <cstring>
-
-int CompareNames(char nume1[50], char nume2[50]);
-int CompareMath(float n1, float n2);
-int CompareEnglish(float n1, float n2);
-int CompareHistory(float n1, float n2);
-int CompareAverage(float n1, float n2);
