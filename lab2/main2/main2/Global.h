5c2acbd8a9178a6f917a0c90ed3ee6a57471f763
Darie Mihnea A2 <153534353+MihneaDarie@users.noreply.github.com>
2024-03-07 15:06:26 +0200

    Delete lab2/main2/main2 directory

--- lab2/main2/main2/Global.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "students.h"
-
-int CompareEGrade(students* a, students* b);
-int CompareMGrade(students* a, students* b);
-int CompareHGrade(students* a, students* b);
-int CompareAverageGrade(students* a, students* b);
-int CompareName(students* a, students* b);
