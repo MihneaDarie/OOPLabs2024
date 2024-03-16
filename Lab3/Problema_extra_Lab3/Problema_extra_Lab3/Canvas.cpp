0276a322fb1860ee0060afb2a52a10de61c83509
Darie Mihnea A2 <153534353+MihneaDarie@users.noreply.github.com>
2024-03-16 15:20:41 +0200

    Problema_extra_Lab3

---
+++ Lab3/Problema_extra_Lab3/Problema_extra_Lab3/Canvas.cpp
@@ -0,0 +1,53 @@
+#include "Canvas.h"
+
+void Canvas::clear()
+{
+    int i, j;
+    for (i = 0; i < h; i++)
+        for (j = 0; j < l; j++)
+            DrawingTable[i][j] = ' ';
+}
+
+Canvas::Canvas(int lines, int columns)
+{
+    this->h = lines;
+    this->l = columns;
+
+    int i;
+    DrawingTable = new char* [h + 5];
+    for (i = 0; i < h; i++)
+        DrawingTable[i] = new char[l + 5];
+    this->clear();
+}
+
+void Canvas::set_pixel(int x, int y, char value)
+{
+    DrawingTable[y][x]=value;
+}
+
+// tuples of x, y, value
+void Canvas::set_pixels(int count, ...)
+{
+    int x, y; char val;
+    va_list param;
+    va_start(param, count*3 );
+    for (int i = 0; i < count; i++)
+    {
+        x = va_arg(param, int);
+        y = va_arg(param, int);
+        val = va_arg(param, char);
+        DrawingTable[y][x] = val;
+    }
+    va_end(param);
+}
+
+void Canvas::print() const 
+{
+    int i, j;
+    for (i = 0; i < h; i++)
+    {
+        for (j = 0; j < l; j++)
+            printf("%c",DrawingTable[i][j]);
+        printf("\n");
+    }
+}
---
+++ Lab3/Problema_extra_Lab3/Problema_extra_Lab3/Canvas.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdarg.h>
+
+class Canvas 
+{
+    char** DrawingTable;
+    int h, l;
+public:
+    // contructor will call the clear method
+    Canvas(int lines, int columns);
+
+    // set the character at the position x and y to value
+    void set_pixel(int x, int y, char value);
+
+    // tuples of x, y, value
+    void set_pixels(int count, ...);
+
+    void clear();
+    void print() const;
+};
